9f993aa815484f8966b8ce1df5bc802839e21615
WMXPY <wm@mengw.io>
2022-03-16 22:04:18 -0500

    release with config

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,39 @@
  * @namespace StorybookConfig
  * @description Index
  */
+
+export type StorybookConfigOptions = {
+
+    readonly stories: string[];
+};
+
+export const createStorybookConfig = (options: StorybookConfigOptions): any => {
+
+    return {
+        stories: options.stories,
+        addons: [
+            '@storybook/addon-a11y',
+            "@storybook/addon-essentials",
+            '@storybook/addon-storysource',
+            '@storybook/addon-docs'
+        ],
+        webpackFinal: async (config: any) => {
+
+            config.module.rules.push(
+                {
+                    test: /\.tsx?$/,
+                    loader: require.resolve('babel-loader'),
+                    options: {
+                        presets: [
+                            "@babel/typescript",
+                            "@babel/react",
+                        ],
+                    },
+                }
+            );
+
+            config.resolve.extensions.push('.ts', '.tsx');
+            return config;
+        },
+    };
+};
